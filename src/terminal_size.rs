5120f6aedaae2ab7a3a466a2be85b0736dac3f32
Andreas Stührk <andy@hammerhartes.de>
2024-01-23 23:36:46 +0100

    Use libc::winsize

--- src/terminal_size.rs
+++ src/terminal_size.rs
@@ -3,25 +3,17 @@
 
 use std::os::fd::AsRawFd;
 
-use libc::{c_ushort, ioctl, TIOCGWINSZ};
-
-#[repr(C)]
-struct TermSize {
-    row: c_ushort,
-    col: c_ushort,
-    x: c_ushort,
-    y: c_ushort,
-}
+use libc::{ioctl, winsize, TIOCGWINSZ};
 
 /// Get the size (columns, rows) of the terminal.
 pub fn terminal_size(fd: &impl AsRawFd) -> std::io::Result<(u16, u16)> {
     unsafe {
-        let mut size: TermSize = std::mem::zeroed();
+        let mut size: winsize = std::mem::zeroed();
         let result = ioctl(fd.as_raw_fd(), TIOCGWINSZ, std::ptr::addr_of_mut!(size));
         if result == -1 {
             Err(std::io::Error::last_os_error())
         } else {
-            Ok((size.col, size.row))
+            Ok((size.ws_col, size.ws_row))
         }
     }
 }
